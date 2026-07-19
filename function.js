ad3046b59641b5dcf32a84b2f8f131d2a2ee87a0
santhanaluxmy <santhanaluxmy20@gmail.com>
2026-07-19 21:41:42 +0530

    all function

---
+++ function.js
@@ -0,0 +1,101 @@
+// 1. Declare a function fullName and print your full name
+
+function fullName() {
+  console.log("Santhana Luxmy");
+}
+
+fullName();
+
+
+// 2. Function with parameters
+
+function fullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+console.log(fullName("Santhana", "Luxmy"));
+
+
+// 3. Add two numbers
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addNumbers(10, 20));
+
+
+// 4. Area of Rectangle
+
+function areaOfRectangle(length, width) {
+  return length * width;
+}
+
+console.log(areaOfRectangle(10, 5));
+
+
+// 5. Perimeter of Rectangle
+
+function perimeterOfRectangle(length, width) {
+  return 2 * (length + width);
+}
+
+console.log(perimeterOfRectangle(10, 5));
+
+
+// 6. Volume of Rectangular Prism
+
+function volumeOfRectPrism(length, width, height) {
+  return length * width * height;
+}
+
+console.log(volumeOfRectPrism(10, 5, 2));
+
+
+// 7. Area of Circle
+
+function areaOfCircle(radius) {
+  return Math.PI * radius * radius;
+}
+
+console.log(areaOfCircle(7));
+
+
+// 8. Circumference of Circle
+
+function circumOfCircle(radius) {
+  return 2 * Math.PI * radius;
+}
+
+console.log(circumOfCircle(7));
+
+
+// 9. Celsius to Fahrenheit
+
+function convertCelsiusToFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+console.log(convertCelsiusToFahrenheit(30));
+
+
+// 10. BMI Calculator
+
+function calculateBMI(weight, height) {
+  let bmi = weight / (height * height);
+
+  if (bmi < 18.5) {
+    return `BMI: ${bmi.toFixed(2)} - Underweight`;
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return `BMI: ${bmi.toFixed(2)} - Normal weight`;
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    return `BMI: ${bmi.toFixed(2)} - Overweight`;
+  } else {
+    return `BMI: ${bmi.toFixed(2)} - Obese`;
+  }
+}
+
+console.log(calculateBMI(70, 1.75));
+console.log(calculateBMI(45, 1.70));
+console.log(calculateBMI(85, 1.70));
+console.log(calculateBMI(110, 1.70));
