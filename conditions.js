8ac3dc788dbd5344cc62c65fd6f3ad3729e84e34
santhanaluxmy <santhanaluxmy20@gmail.com>
2026-07-22 15:52:19 +0530

    Files added

---
+++ conditions.js
@@ -0,0 +1,8 @@
+let age = Number(prompt("Enter your age:"));
+
+if (age >= 18) {
+    console.log("You are old enough to drive.");
+} else {
+    let yearsLeft = 18 - age;
+    console.log(`You are left with ${yearsLeft} years to drive.`);
+}
